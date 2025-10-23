f6e71bf6ba86cc9c52e2583f956df9158ccac048
amarpakhare07 <AmarWaman.Pakhare@cognizant.com>
2025-10-23 15:30:44 +0530

    Updated the navbar profile routes

--- src/app/models/interfaces.ts
+++ src/app/models/interfaces.ts
@@ -37,20 +37,7 @@ export interface DashboardSummary {
   };
 }
 
-// export interface Course {
-//   courseID: number;
-//   title: string;
-//   description: string;
-//   syllabus: string;
-//   level: string;
-//   language: string;
-//   duration: number;
-//   thumbnailURL: string;
-//   categoryID: number;
-//   published: boolean;
-//   rating: number;
-//   reviewCount: number;
-// }
+
 
 
 export interface Category {
