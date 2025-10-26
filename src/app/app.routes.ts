cf57454646a5b1d86e2cfefd7b15dfa5b2d2635c
amarpakhare07 <AmarWaman.Pakhare@cognizant.com>
2025-10-26 09:32:42 +0530

    Add routing for About, Contact Us, and Quiz components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,6 +28,12 @@ import { ProfileComponent } from './components/profile/user-profile/user-profile
 import { PublicProfileComponent } from './components/profile/public-profile/public-profile';
 import { PhotoProfileComponent } from './components/profile/photo-profile/photo-profile';
 import { QuizSummaryComponent } from './components/student/quiz-summary/quiz-summary';
+import { About } from './components/shared/about/about';
+import { ContactUs } from './components/shared/contact-us/contact-us';
+import { QuizResultsComponent } from './components/quiz/quiz-result/quiz-result';
+import { quizEnrolledGuard } from './services/quiz-enrolled.guard';
+import { QuizAttemptComponent } from './components/quiz/quiz-attempt/quiz-attempt';
+import { QuizListComponent } from './components/quiz/quiz-list/quiz-list';
 export const routes: Routes = [
   // Public Routes
   { path: 'login', component: LoginComponent, canActivate: [publicGuard] },
